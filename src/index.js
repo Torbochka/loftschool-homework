85b87ed56fbea644c2a69b7a660bf743ca7fa647
Alexander Tobek <sasha.torbek@yandex.ru>
2019-02-07 22:56:16 +0300

    hw2

---
+++ src/index.js
@@ -0,0 +1,119 @@
+/* ДЗ 2 - работа с массивами и объеектами */
+
+/*
+ Задание 1:
+
+ Напишите аналог встроенного метода forEach для работы с массивами
+ Посмотрите как работает forEach и повторите это поведение для массива, который будет передан в параметре array
+ */
+function forEach(array, fn) {
+    for (let i = 0; i < array.length; i++) {
+        fn(array[i], i, array);
+    }
+}
+
+/*
+ Задание 2:
+
+ Напишите аналог встроенного метода map для работы с массивами
+ Посмотрите как работает map и повторите это поведение для массива, который будет передан в параметре array
+ */
+function map(array, fn) {
+    let cloned = [];
+
+    for (let i = 0; i < array.length; i++) {
+        cloned.push(fn(array[i], i, array));
+    }
+
+    return cloned;
+}
+
+/*
+ Задание 3:
+
+ Напишите аналог встроенного метода reduce для работы с массивами
+ Посмотрите как работает reduce и повторите это поведение для массива, который будет передан в параметре array
+ */
+function reduce(array, fn, initial) {
+    let cloned = array;
+    let res = initial ? initial : cloned[0];
+    let s = initial ? 0 : 1;
+
+    for (let i = s; i < cloned.length; i++) {
+        res = fn(res, cloned[i], i, cloned);
+    }
+
+    return res;
+}
+
+/*
+ Задание 4:
+
+ Функция должна перебрать все свойства объекта, преобразовать их имена в верхний регистр и вернуть в виде массива
+
+ Пример:
+   upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
+ */
+function upperProps(obj) {
+    return Object.keys(obj).map(value => value.toUpperCase());
+}
+
+/*
+ Задание 5 *:
+
+ Напишите аналог встроенного метода slice для работы с массивами
+ Посмотрите как работает slice и повторите это поведение для массива, который будет передан в параметре array
+ */
+function slice(array, from = 0, to = array.length) {
+    let len = array.length;
+    let cloned = [];
+    let begin = from;
+    let end = to;
+
+    if (len + to < 0 || len < from) {
+        return cloned;
+    }
+
+    if (to > len) {
+        end = len;
+    }
+
+    if (len + to < len) {
+        end = len + to;
+    }
+
+    if (len + from < 0) {
+        begin = 0;
+    }
+
+    for (let i = begin; i < end; i++) {
+        cloned.push(array[i]);
+    }
+
+    return cloned;
+}
+
+/*
+ Задание 6 *:
+
+ Функция принимает объект и должна вернуть Proxy для этого объекта
+ Proxy должен перехватывать все попытки записи значений свойств и возводить это значение в квадрат
+ */
+function createProxy(obj) {
+    return new Proxy(obj, {
+        set(target, prop, value) {
+            target[prop] = value*value;
+            
+            return true;
+        }
+    });
+}
+
+export {
+    forEach,
+    map,
+    reduce,
+    upperProps,
+    slice,
+    createProxy
+};
